57d4ec80be502a34520a69d41fe8b5ea5828831d
Jess <jessicapeter92@gmail.com>
2021-09-13 20:56:15 +0100

    compartmentalized css and added web fonts

--- webdev/dev/components/project.tsx
+++ webdev/dev/components/project.tsx
@@ -7,33 +7,38 @@ type Props = {
 	info: string, 
 	responsibilities: string,
 	image: string, 
-	years: [number]|[number, number]
+	years: [number]|[number, number],
+	link: {"display": string, "address": string},
+	alt: string
 }
 
-const Project = ({ org, title, info, responsibilities, image, years }: Props) => (
+const Project = ({ org, title, info, responsibilities, image, years, link, alt }: Props) => (
     <>
     	<div className={styles.projectGrid}>
-    		<div>
+    		<div className={styles.projectYear}>
     			<h2>{years[0]}<br/>{years.length === 1 || `- ${years[1]}`}</h2>
     		</div>
-    		<div>
+    		<div className={styles.projectHeader}>
     			<h3>{title}</h3>
     			<h4>{org}</h4>
-    		</div>
-    		<div className={styles.arrowDown}>
+    			<p>
+    				<a href={link.address} target="_blank">{link.display}</a>
+    			</p>
     		</div>
     		<div className={styles.imgHolder}>
-     			<Image src={image} alt="me" layout='fill' objectFit='cover' />
+     			<Image src={image} alt={alt} layout='fill' objectFit='cover' />
     		</div>
-    		<div></div>
-	     	<div>
+	     	<div className={styles.description}>
 		    	<p>{info}</p>
 		    </div>
 		    <div className={styles.label}>
 		    	<p className={styles.responsibilities}>I did:</p>
 		    </div>
-		    <div>
-		    	<p className={styles.responsibilities}>{responsibilities}</p>
+		    <div className={styles.resp}>
+		    	<p className={styles.responsibilities}>
+		    		<span className={styles.responsibilities + " " + styles.hideOnBig} style={{"fontWeight": "bold"}}>I did: </span>
+		    		{responsibilities}
+		    	</p>
 		    </div>
     	</div>
     </>
