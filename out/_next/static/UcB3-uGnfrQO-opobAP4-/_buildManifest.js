ba65380e47e12733f1d518d36f62a848557f76ef
Jess <jessicapeter92@gmail.com>
2021-09-13 21:53:09 +0100

    removed out from gitignore

---
+++ out/_next/static/UcB3-uGnfrQO-opobAP4-/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/ac6f680c04de0983dacd.css","static/chunks/pages/index-13be348457eeaa10cd58.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
