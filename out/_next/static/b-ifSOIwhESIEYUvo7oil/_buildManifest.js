cd9bc3484228b1688ff0ae1f643b76af754d3427
Jess <jessicapeter92@gmail.com>
2022-02-02 21:40:38 +0000

    Deploy Next.js to gh-pages

---
+++ out/_next/static/b-ifSOIwhESIEYUvo7oil/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/f157d3950ea4b4cdabb0.css","static/chunks/pages/index-7dda87525077b9c4660c.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- out/_next/static/c5oh7FbaFE7-cFxXEPsBe/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/f157d3950ea4b4cdabb0.css","static/chunks/pages/index-d738821c235939de4c89.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
