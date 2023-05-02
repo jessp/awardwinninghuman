51555e2b21563b5473c833509f36dc0148f3b833
jessp <jessicapeter92@gmail.com>
2023-05-02 13:53:29 +0100

    animation works, though small

--- components/Modello.tsx
+++ components/Modello.tsx
@@ -1,7 +1,7 @@
 // @ts-nocheck
 
 import * as THREE from "three";
-import React, { useRef, useEffect } from "react";
+import React, { useRef } from "react";
 import { useGLTF, useAnimations } from "@react-three/drei";
 import { GLTF } from "three-stdlib";
 
@@ -20,31 +20,25 @@ type GLTFActions = Record<ActionName, THREE.AnimationAction>;
 
 export function Model(props: JSX.IntrinsicElements["group"]) {
   const group = useRef<THREE.Group>();
-  const { nodes, materials, animations } = useGLTF("/2may.gltf") as GLTFResult;
+  const { nodes, materials, animations } = useGLTF(
+    "/2may_compressed.glb"
+  ) as GLTFResult;
   const { actions } = useAnimations<GLTFActions>(animations, group);
-
-
   return (
-    <group 
-    ref={group}
-    {...props} 
-    dispose={null}
-    onClick={(e) => actions["Hands on Hips"].play()}>
+    <group ref={group} {...props} dispose={null} onClick={(e) => actions["Hands on Hips"].play()}>
       <group name="Scene">
-        <group name="metarig" scale={0.01}>
+        <group name="metarig" scale={0.003}>
           <primitive object={nodes.spine} />
           <skinnedMesh
             name="acc_hair_punkin"
             geometry={nodes.acc_hair_punkin.geometry}
             material={nodes.acc_hair_punkin.material}
-            material-color="green"
             skeleton={nodes.acc_hair_punkin.skeleton}
           />
           <skinnedMesh
             name="casual_Female_K"
             geometry={nodes.casual_Female_K.geometry}
             material={nodes.casual_Female_K.material}
-            material-color="green"
             skeleton={nodes.casual_Female_K.skeleton}
           />
         </group>
@@ -54,13 +48,13 @@ export function Model(props: JSX.IntrinsicElements["group"]) {
           receiveShadow
           geometry={nodes.trophy_base.geometry}
           material={nodes.trophy_base.material}
-          material-color="green"
           rotation={[Math.PI / 2, 0, 0]}
-          scale={0.01}
+          scale={0.005}
         />
       </group>
     </group>
   );
 }
 
-useGLTF.preload("/2may.gltf");
+useGLTF.preload("/2may_compressed.glb");
+
