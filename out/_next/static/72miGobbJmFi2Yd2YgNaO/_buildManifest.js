f01ac87b91218187ca5a7e203d5cdff57029a976
jessp <jessicapeter92@gmail.com>
2023-05-08 21:22:15 +0100

    Deploy Next.js to gh-pages

--- out/_next/static/72miGobbJmFi2Yd2YgNaO/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/89f88018a0ed16dd.css","static/chunks/pages/index-348efd315865c2ed.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
