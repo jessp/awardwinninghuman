2d257f305424e1bba7a91140fbde662af019fec5
jessp <jessicapeter92@gmail.com>
2023-05-08 20:58:10 +0100

    added favicons

--- pages/index.tsx
+++ pages/index.tsx
@@ -41,7 +41,10 @@ const Home: NextPage = () => {
       <title>Jess Peter | Designer</title>
       <meta name="description" content="Award-winning Human Jess Peter" />
       <meta name="viewport" content="width=device-width, initial-scale=1"/>
-      <link rel="icon" href="/assets/favicon-01.png" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+      <link rel="manifest" href="/site.webmanifest"/>
     </Head>
 
     <main>
