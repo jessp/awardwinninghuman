c0b9ec05e96b24030fe1a77f3d3529f8b5068688
jessp <jessicapeter92@gmail.com>
2023-06-09 13:03:01 +0200

    removed env again

--- components/scene.tsx
+++ components/scene.tsx
@@ -1,7 +1,6 @@
 // @ts-nocheck
 import { Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
-import { Environment } from "@react-three/drei";
 import {Trophy} from './trophy';
 
 type Props = {
@@ -12,7 +11,6 @@ const Scene = ({ isWork }: Props) => (
       <Canvas shadows >
         <Suspense fallback={null}>
           <Trophy isWork={isWork}/>
-          <Environment preset="sunset"  />
         </Suspense>
       </Canvas>
 )
