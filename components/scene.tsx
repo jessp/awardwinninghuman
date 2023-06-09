601c74ed5808990bf16ef16e53f39854e4050f98
jessp <jessicapeter92@gmail.com>
2023-06-09 13:01:27 +0200

    switched to in between version of drei

--- components/scene.tsx
+++ components/scene.tsx
@@ -1,6 +1,7 @@
 // @ts-nocheck
 import { Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
+import { Environment } from "@react-three/drei";
 import {Trophy} from './trophy';
 
 type Props = {
@@ -11,6 +12,7 @@ const Scene = ({ isWork }: Props) => (
       <Canvas shadows >
         <Suspense fallback={null}>
           <Trophy isWork={isWork}/>
+          <Environment preset="sunset"  />
         </Suspense>
       </Canvas>
 )
