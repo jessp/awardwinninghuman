b36da1d2aabae3d5e7d8e95faa11b6c9ce273459
jessp <jessicapeter92@gmail.com>
2023-11-17 21:26:16 +0000

    progress on mobile

--- components/header.tsx
+++ components/header.tsx
@@ -8,13 +8,25 @@ const Header = () => (
     <div className={styles.heroStarter}>
     	<div className={`${styles.titleHolder} ${styles.backText}`}>
     		<img
+    		className={"hideOnSmall"}
 			src={"/big_back_vector.svg"}
+			alt=""
+			/>
+			<img
+			className={"hideOnMed"}
+			src={"/small_back_vector.svg"}
 			alt=""
 			/>
 	    </div>
 	    <div className={`${styles.titleHolder} ${styles.frontText}`}>
 	    	<img
+	    	className={"hideOnSmall"}
 			src={"/big_front_vector.svg"}
+			alt=""
+			/>
+			<img
+			className={"hideOnMed"}
+			src={"/small_front_vector.svg"}
 			alt=""
 			/>
 	    </div>
